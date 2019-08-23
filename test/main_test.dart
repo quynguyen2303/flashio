097af1db65d15e7dd9d4dd05390a6f96fab0a995
23editorcs <23editorcs@gmail.com>
2019-08-23 17:45:48 +0700

    Create main_test.dart

---
+++ test/main_test.dart
@@ -0,0 +1,19 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flashio/main.dart';
+import '../lib/pages/ConversationPageList.dart';
+
+void main() {
+  testWidgets('Main UI Test', (WidgetTester tester) async {
+    // Build our app and trigger a frame
+    await tester.pumpWidget(Flashio());
+
+    expect(find.byType(ConversationPageList), findsOneWidget);
+  });
+}
