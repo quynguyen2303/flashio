c572f5c265d507194876b7bc8deb6ae8caf07d87
23editorcs <23editorcs@gmail.com>
2019-08-23 17:46:09 +0700

    Update ConversationPageList.dart

--- lib/pages/ConversationPageList.dart
+++ lib/pages/ConversationPageList.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'ConversationPage.dart';
 
 class ConversationPageList extends StatefulWidget {
+  const ConversationPageList();
   @override
   _ConversationPageListState createState() => _ConversationPageListState();
 }
