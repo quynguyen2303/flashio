3b1c1d06c3150c4a11794e58f6eaa56f3b98ed94
23editorcs <23editorcs@gmail.com>
2019-08-23 17:07:13 +0700

    Create ConversationPage.dart

---
+++ lib/pages/ConversationPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/chat_app_bar.dart';
+import '../widgets/chat_list.dart';
+import '../widgets/input.dart';
+
+class ConversationPage extends StatefulWidget {
+  @override
+  _ConversationPageState createState() => _ConversationPageState();
+}
+
+class _ConversationPageState extends State<ConversationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: ChatAppBar(), // Custom App Bar
+          body: Stack(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  ChatListWidget(), // Chat list
+                  InputWidget(), // The input section
+                ],
+              )
+            ],
+          )),
+    );
+  }
+}
