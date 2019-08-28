2e111b21dd9e3f6d17bfb21ad33f77eca2da56d6
quynguyen2303 <nguyenphuquy2303@gmail.com>
2019-08-28 10:05:04 +0700

    Update the navigation pill

--- lib/widgets/navigation_pill.dart
+++ lib/widgets/navigation_pill.dart
@@ -5,14 +5,17 @@ import '../config/Palette.dart';
 class NavigationPill extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      margin: EdgeInsets.only(top: 10, bottom: 10),
-      height: 5,
-      decoration: new BoxDecoration(
-        color: Palette.accentColor,
-        shape: BoxShape.rectangle,
-        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+    return Center(
+      child: Wrap(children:  [Container(
+          width: 50,
+          margin: EdgeInsets.only(top: 10, bottom: 10),
+          height: 5,
+          decoration: new BoxDecoration(
+            color: Palette.accentColor,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+        ),]
       ),
     );
   }
